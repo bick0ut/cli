d6494c045e55da6ec0c0200a53852e1771f83a76
bick0ut <chennick511@gmail.com>
2023-12-12 17:19:14 -0500

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::{fs, io};
+use clap::Parser;
 
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
 fn main() {
     let mut input = String::new();
     loop {
@@ -14,27 +21,38 @@ fn main() {
 
         match input.as_str().trim() {
             "exit" => break,
-            "help" => println!(
-                "ls - list files in current directory\n\
-                help - lists possible commands\n\
-                exit - exits the program"
-            ),
-            "ls" => {
-                let paths = fs::read_dir("./").unwrap();
-
-                for path in paths {
-                    let path = path.unwrap();
-                    let md = path.metadata();
-                    if md.unwrap().is_file(){
-                        println!("{}", path.path().display().to_string().trim_start_matches("./"));
-                    } else {
-                        println!("/{}", path.path().display().to_string().trim_start_matches("./"));
-                    }
-
-                }
-            },
+            "help" => print_help(),
+            "ls" => print_ls(),
             _n => println!("Unknown command {_n}")
         }
 
+    }
+}
+
+fn match_command(string: String){
+
+}
+fn print_help(){
+    println!(
+        "ls - list files in current directory\n\
+                help - lists possible commands\n\
+                exit - exits the program"
+    )
+}
+
+fn print_ls(){
+    {
+        let paths = fs::read_dir("./").unwrap();
+
+        for path in paths {
+            let path = path.unwrap();
+            let md = path.metadata();
+            if md.unwrap().is_file() {
+                println!("{}", path.path().display().to_string().trim_start_matches("./"));
+            } else {
+                println!("/{}", path.path().display().to_string().trim_start_matches("./"));
+            }
+
+        }
     }
 }
